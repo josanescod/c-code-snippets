6beb5a129e9c0cecc1fcdc6ec0c9f9cda14ffb38
josan <josanescod@gmail.com>
2021-05-14 16:40:41 +0200

    chapter 1

--- c1/functions.c
+++ c1/functions.c
@@ -12,12 +12,23 @@ int larger(int a, int b)
 void greet(){
     puts ("Hello mr Smith");
 }
+
+//function chaining assignments
+int chain_values(int c,int d, int e){
+    c = 2;
+    d = c = 4;
+    e = 3;
+
+
+    return c + d + e ;
+}
 int main()
 {
 
     int greatest = larger(79, 86);
     printf("%i is the greatest!\n", greatest);
     greet();
-
+    int modify_3_values = chain_values(0,0,0);
+    printf("total is: %i\n", modify_3_values);
     return 0;
 }
