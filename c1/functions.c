028f15aee72b4f4307f3cdb27e23759c67035045
josan <josanescod@gmail.com>
2021-05-14 16:32:52 +0200

    chapter 1

--- c1/functions.c
+++ c1/functions.c
@@ -8,11 +8,16 @@ int larger(int a, int b)
     return b;
 }
 
+//function void example
+void greet(){
+    puts ("Hello mr Smith");
+}
 int main()
 {
 
     int greatest = larger(79, 86);
     printf("%i is the greatest!\n", greatest);
+    greet();
 
     return 0;
 }
