6be626bfabf2ea344f2757631b4e17ed7cc12445
josan <josanescod@gmail.com>
2021-05-14 08:00:54 +0200

    chapter 1

--- c1/evaluate_card.c
+++ c1/evaluate_card.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
+int main()
+{
 
-char card_name[3];
-puts ("Enter the card_name: ");
-scanf("%2s", card_name);
-int val = 0;
+    char card_name[3];
+    puts("Enter the card_name: ");
+    scanf("%2s", card_name);
+    int val = 0;
 
-if (card_name[0] == 'K') {
-    val = 10;
+    if (card_name[0] == 'K')
+    {
+        val = 10;
+    }
+    else if (card_name[0] == 'Q')
+    {
+        val = 10;
+    }
+    else if (card_name[0] == 'J')
+    {
+        val = 10;
+    }
+    else if (card_name[0] == 'A')
+    {
+        val = 10;
+    }
+    else
+    {
+        val = atoi(card_name);
+    }
 
-} else if (card_name[0] == 'Q'){
-    val = 10;
-} else if (card_name[0] == 'J'){
-    val = 10;
-} else if (card_name[0] == 'A'){
-    val = 10;
-}else {
-    val = atoi(card_name);
-}
-printf("The card value is : %i\n", val);
+    printf("The card value is : %i\n", val);
+    if ((val > 2) && (val < 7))
+        puts("Count has gone up");
+    else if (val == 10)
+        puts("Count has gone down");
+    else
+        puts("Value is 0");
 
-
-return 0;
+    return 0;
 }
