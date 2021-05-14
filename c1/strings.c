b6c19021be96ab57a76d761c204c25117e342a5b
josan <josanescod@gmail.com>
2021-05-14 07:47:37 +0200

    chapter 1

---
+++ c1/strings.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+
+    char hello[10]= {'h','e','l','l','o','w','o','r','l','d'};
+    int count = 0;
+    int countdown = 9;
+    puts(hello);
+    printf ("%s\n",hello);
+    printf ("%c\n",hello[5]);
+
+    while (count < 10){
+
+        printf("%c %c\n",hello[count],hello[countdown]);
+        count++;
+        countdown--;
+    }
+
+    return 0;
+}
