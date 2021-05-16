c375dd4f73cd8a8a9a6a6bbe8a24f697ce2e1b76
josan <josanescod@gmail.com>
2021-05-16 08:20:47 +0200

    c2 pointers

--- c2/pointers1.c
+++ c2/pointers1.c
@@ -6,16 +6,25 @@ constants
 code
 
 
+
+-get the address variable
+get the address variable printf("x lives at %p\n", &x);
+-store the address variable
+int *address_of_x = &x;
+-read the contents of an address
+int value_stored = *address_of_x;
+-change content of an address
+*address_of_x = 100
 */
 
 #include <stdio.h>
 
 //function
-void go_south_east(int lat, int lon)
+void go_south_east(int *lat, int *lon)
 {
     //stored in stack
-    lat = lat - 1;
-    lon = lon + 1;
+    *lat = *lat - 1;
+    *lon = *lon + 1;
 }
 
 //stored in globals
@@ -27,7 +36,7 @@ int main()
     //stored in stack
     int latitude = 32;
     int longitude = -64;
-    go_south_east(latitude, longitude);
+    go_south_east(&latitude, &longitude);
     printf("Avast! now at: [%i,%i]\n", latitude, longitude);
     return 0;
 }
