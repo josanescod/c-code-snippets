687dc025bd9f00b0a832247166978645986652fa
josan <josanescod@gmail.com>
2021-06-16 12:14:13 +0200

    fortune_cookie.c

---
+++ c2/fortune_cookie.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/* function */
+void fortune_cookie(char msg[])
+{
+	printf("Message reads: %s\n", msg);
+	printf("msg occupies %i bytes\n", sizeof(msg));
+
+}
+
+int main()
+{
+	
+	char quote[] = "Cookies make you fat";
+	fortune_cookie(quote);
+
+	return 0;
+}
+
